e9003af2cd7ecc302356458a47c3374fb2decb2a
kamil116rus <kamil.hakimov78@gmail.com>
2023-09-02 02:06:37 +0300

    connecting my DB

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,24 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    public User getUser(String model, int series) {
+        String hql = "FROM User user LEFT OUTER JOIN FETCH user.car WHERE user.car.model=:model and user.car.series=:series";
+
+        return sessionFactory.getCurrentSession().createQuery(hql, User.class).setParameter("model", model).setParameter("series", series).uniqueResult();
+    }
 }
